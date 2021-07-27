95259af0c00fef266def80ccacff6b7cf0d5bfa5
My <My@ANNEX-188>
2021-07-27 11:15:54 +0900

    초기 커밋.

---
+++ Main.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Main.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            #region ver1
+
+            Uri uri = new Uri("/Page1.xaml", UriKind.Relative);
+            NavigationService.Navigate(uri);
+
+            #endregion
+        }
+    }
+}
